04b51bae8cb03944d89e18ad6e7f4e6b9260d080
Arne Bahlo <arne@axiom.co>
2021-11-11 12:15:18 +0100

    Add workgate

---
+++ workgate/doc.go
@@ -0,0 +1,3 @@
+// Package workgate provides a struct to ensure no more than N tasks are ever
+// ongoing.
+package workgate
---
+++ workgate/example_test.go
@@ -0,0 +1,49 @@
+//nolint: goconst // using consts in examples makes them harder to read
+package workgate
+
+import "fmt"
+
+func ExampleWorkGate_Do() {
+	wg := New(10)
+	returnValue, err := wg.Do(func() (interface{}, error) {
+		// Do some work (blocking)
+		return "foo", nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(returnValue)
+	// Output: foo
+}
+
+func ExampleWorkGate_DoAsync() {
+	wg := New(10)
+	done := make(chan struct{})
+	wg.DoAsync(func() {
+		// Do some work (async)
+		fmt.Println("foo")
+		close(done)
+	})
+	<-done
+	// Output: foo
+}
+
+func ExampleWorkGate_TryDo() {
+	wg := New(10)
+
+	// Make sure the gate is full
+	free := make(chan struct{})
+	for i := 0; i < 10; i++ {
+		wg.DoAsync(func() {
+			<-free
+		})
+	}
+	defer close(free)
+
+	_, err := wg.TryDo(func() (interface{}, error) {
+		// Do some work (blocking)
+		return "foo", nil
+	})
+	fmt.Println(err)
+	// Output: gate full
+}
